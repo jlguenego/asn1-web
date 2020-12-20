a8f54dce9d18875a21bfd98e3a287607d69d5928
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-20 15:34:11 +0100

    added logo and routing

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
+import { LegalComponent } from './routes/legal/legal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'legal', component: LegalComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- front/src/app/layout/layout.module.ts
+++ front/src/app/layout/layout.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, BodyComponent, FooterComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule],
+  exports: [HeaderComponent, BodyComponent, FooterComponent],
 })
-export class LayoutModule { }
+export class LayoutModule {}
