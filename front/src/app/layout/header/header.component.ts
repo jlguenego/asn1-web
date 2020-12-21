22cd172dbd1dd184309dc9e6407626b6af801399
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-21 19:58:40 +0100

    added font awesome

--- front/src/app/layout/header/header.component.ts
+++ front/src/app/layout/header/header.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  faGithub = faGithub;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- front/src/app/layout/layout.module.ts
+++ front/src/app/layout/layout.module.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [HeaderComponent, BodyComponent, FooterComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   exports: [HeaderComponent, BodyComponent, FooterComponent],
 })
 export class LayoutModule {}
