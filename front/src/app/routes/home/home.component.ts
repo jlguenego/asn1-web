988fcd600f84d6dca008bce567973f87bfac43b0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-21 19:12:20 +0100

    added generate

--- front/src/app/routes/home/home.component.ts
+++ front/src/app/routes/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { ASN1, EncodingRule } from '@jlguenego/asn.1';
-import { derDefault } from 'src/data/default';
+import { ASN1, EncodingRule, DEREncode } from '@jlguenego/asn.1';
+import { asn1Default, asn1TypeDefault, derDefault } from 'src/data/default';
 
 @Component({
   selector: 'app-home',
@@ -12,6 +12,8 @@ export class HomeComponent implements OnInit {
   f = new FormGroup({
     der: new FormControl(derDefault),
     json: new FormControl(''),
+    asn1: new FormControl(asn1Default),
+    type: new FormControl(asn1TypeDefault),
   });
 
   constructor() {}
@@ -25,5 +27,25 @@ export class HomeComponent implements OnInit {
       format: 'hex',
     });
     this.f.controls.json.setValue(JSON.stringify(result, null, 2));
+  }
+
+  validate(): void {
+    console.log('validate');
+    if (!this.f.controls.json.value) {
+      this.parse();
+    }
+    const module = ASN1.getModuleFromStr(this.f.controls.asn1.value);
+    const validatedMsg = ASN1.validate(
+      module,
+      JSON.parse(this.f.controls.json.value),
+      this.f.controls.type.value
+    );
+    this.f.controls.json.setValue(JSON.stringify(validatedMsg, null, 2));
+  }
+
+  generate(): void {
+    console.log('generate');
+    const derMessage = DEREncode(JSON.parse(this.f.controls.json.value));
+    this.f.controls.der.setValue(derMessage);
   }
 }
--- front/src/data/default.ts
+++ front/src/data/default.ts
@@ -1,9 +1,35 @@
-export const derDefault = `30 # Sequence
-  13 # Seq Length
-    02 # Integer
-      01 # Integer Length
-        05 # 5
-    16 # IA5String (ASCII string)
-      0e # String length (14)
-        41 6e 79 62 6f 64 79 20 74 68 65 72 65 3f # String content ("Anybody there?")
+export const derDefault = `30 # Sequence constructed (WelcomeMsg)
+1D # Length (29)
+  02 # Type Integer (id)
+    01 # Length 1
+      12 # 0x12 = 18
+  30 # Sequence constructed (someone)
+    18 # Length (24)
+      1B # Type GeneralString (lastname)
+        07 # Length (7)
+          47 55 45 4E 45 47 4F # value: GUENEGO
+      1B # Type GeneralString (firstname)
+        0A # Length (10)
+          4A 65 61 6E 2D 4C 6F 75 69 73 # value: Jean-Louis
+      01 # Type BOOLEAN (likeCoding)
+        01 # Length (1)
+          FF # TRUE
 `;
+
+export const asn1Default = `HelloProtocol DEFINITIONS ::= BEGIN
+
+Person ::= SEQUENCE {
+    lastname           GeneralString,
+    firstname          GeneralString,
+    likeCoding     BOOLEAN
+}
+
+WelcomeMsg ::= SEQUENCE {
+    id        INTEGER,
+    someone   Person
+}
+
+END
+`;
+
+export const asn1TypeDefault = 'WelcomeMsg';
