16e75f841dbe0e6aaea7c65fc1f78e6f7ba71539
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-31 17:42:58 +0100

    added @jlguenego/lexer for test

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { Group, Rule, Lexer } from '@jlguenego/lexer';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,5 +9,16 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+
+const str = '"asdf"';
+const strRule = new Rule({
+  name: 'string',
+  pattern: /".*"/,
+});
+
+const rules = [strRule];
+const tokens = new Lexer(rules).tokenize(str);
+console.log('tokens: ', tokens);
